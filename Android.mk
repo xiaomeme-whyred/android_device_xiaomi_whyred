e77fd051c5629869d3ab5372766ccff9660d1508
Davide Garberi <dade.garberi@gmail.com>
2019-09-04 23:30:15 +0200

    whyred: Use ifeq instead of ifneq for device inclusion
    Change-Id: If81b0decd4492adbfb2d07570d1f8e771eb082d6

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter whyred,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),whyred)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
