7160d610df232297ec0d43ca793b941d41e6d46e
SakilMondal <mondalsakil@gmail.com>
2020-05-03 11:30:20 +0530

    Revert "whyred: Update TARGET_KERNEL_CONFIG"
    This reverts commit 2113e67aec5a0b1d48d052322ab211c3b80445bb.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_HW_DISK_ENCRYPTION := true
 TARGET_SCREEN_DENSITY := 420
 
 # Kernel
-TARGET_KERNEL_CONFIG := whyred_defconfig
+TARGET_KERNEL_CONFIG := whyred-perf_defconfig
 
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/manifest.xml
