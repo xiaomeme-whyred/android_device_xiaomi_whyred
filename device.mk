3e35a263156fe75e630371b76dd914b9aab6f9e4
srfarias <erikofarias@gmail.com>
2020-05-29 11:58:32 +0000

    Revert "whyred: build Gatekeeper and Keymaster HAL"
    This reverts commit 368fe2eaef7967f67034ac57ca9268ca1db00eaa.
    
    Change-Id: Ie770bedce1360ae6d2238b4ed858df7cd3fd322f

--- device.mk
+++ device.mk
@@ -42,16 +42,6 @@ PRODUCT_COPY_FILES += \
 # FM
 BOARD_HAVE_QCOM_FM := true
 
-# Gatekeeper HAL
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service
-
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
